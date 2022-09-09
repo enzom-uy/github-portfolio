9bc805f8cd4a464bdbe5a61066fa7dad6291ebed
enzom-uy <enzom.dev@gmail.com>
2022-09-08 21:29:44 -0300

    Delete trpc and superjson

--- src/server/router/index.ts
+++
@@ -1,8 +0,0 @@
-// src/server/router/index.ts
-import superjson from "superjson";
-import { createRouter } from "./context";
-
-export const appRouter = createRouter().transformer(superjson);
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
